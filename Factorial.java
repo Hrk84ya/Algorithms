f935ed60d898a8aad325abb5b8fe46ca3cc0fb78
hrk84ya <hrk84ya@gmail.com>
2024-03-02 18:22:16 +0530

    Program for finding factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java. util.Scanner;
+public class Factorial {
+    public static int findFact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*findFact(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        int fact=findFact(n);
+        System.out.println("Factorial of "+n+" is "+fact);
+        sc.close();
+    }
+}
