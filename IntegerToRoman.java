ebadd99bbdd19d09564aa3fd585808b30de5df09
Harsh Kumar <hrk84ya@gmail.com>
2024-09-11 10:27:56 +0530

    Integer to Roman Conversion

---
+++ IntegerToRoman.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        sc.close();
+
+        System.out.println(intToRoman(num));
+        
+    }
+
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        return result.toString();
+    }
+}
